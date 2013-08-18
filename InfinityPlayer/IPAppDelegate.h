00b19037d7f1679607dd4ec22c1edf4d4b637ab7
Justin Martin <lupusdei108@gmail.com>
2013-08-18 15:08:56 -0400

    Initial Commit

---
+++ InfinityPlayer/IPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IPAppDelegate.h
+//  InfinityPlayer
+//
+//  Created by Justin Martin on 8/18/13.
+//  Copyright (c) 2013 JMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InfinityPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InfinityPlayer
+//
+//  Created by Justin Martin on 8/18/13.
+//  Copyright (c) 2013 JMM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IPAppDelegate class]));
+    }
+}
