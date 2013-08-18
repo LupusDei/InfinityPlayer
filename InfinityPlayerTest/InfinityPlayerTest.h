d223b6f186b19dd248b5e4f46095a4400dc6ad15
Justin Martin <lupusdei108@gmail.com>
2013-08-18 15:14:41 -0400

    Initial project commit.

---
+++ InfinityPlayerTest/InfinityPlayerTest.h
@@ -0,0 +1,13 @@
+//
+//  InfinityPlayerTest.h
+//  InfinityPlayerTest
+//
+//  Created by Justin Martin on 8/18/13.
+//  Copyright (c) 2013 JMM. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface InfinityPlayerTest : SenTestCase
+
+@end
---
+++ InfinityPlayerTest/InfinityPlayerTest.m
@@ -0,0 +1,32 @@
+//
+//  InfinityPlayerTest.m
+//  InfinityPlayerTest
+//
+//  Created by Justin Martin on 8/18/13.
+//  Copyright (c) 2013 JMM. All rights reserved.
+//
+
+#import "InfinityPlayerTest.h"
+
+@implementation InfinityPlayerTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in InfinityPlayerTest");
+}
+
+@end
